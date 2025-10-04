8abc9bdc737ce5f711dc6455adde64315258546d
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-10-04 00:31:40 +0000

    sets up react and tailwind for my project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-gray-500 text-center text-4xl font-bold p-6'>Movie Database App</h1>
+    </>
+  )
+}
+
+export default App
